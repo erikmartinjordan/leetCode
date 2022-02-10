2afb982c35056f2711a67ddc27d5a19cb1ec2987
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-02-10 09:58:07 +0100

    Create 155. Min Stack.js

---
+++ 155. Min Stack.js
@@ -0,0 +1,51 @@
+var MinStack = function() {
+    
+    this.stack = []
+    
+}
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    
+    this.stack = [...this.stack, val]
+    
+}
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    
+    this.stack = this.stack.slice(0, this.stack.length - 1)
+    
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    
+    return this.stack[this.stack.length - 1]
+    
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    
+    return Math.min(...this.stack)
+    
+}
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
