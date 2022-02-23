6138c3a58ebe80237ba30bf41b218efe027dd1b8
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-02-23 16:49:34 +0100

    Create 232. Implement Queue using Stacks.js

---
+++ 232. Implement Queue using Stacks.js
@@ -0,0 +1,56 @@
+
+var MyQueue = function() {
+    
+    this.arr = []
+    
+}
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    
+    this.arr = [...this.arr, x]
+    
+}
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    
+    let first = this.arr[0]
+    
+    this.arr = this.arr.slice(1)
+    
+    return first
+    
+}
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    
+    return this.arr[0]
+    
+}
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    
+    return this.arr.length === 0
+    
+}
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
