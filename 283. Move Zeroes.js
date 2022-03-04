7ce15c5fc7f3330020bac42b29958b08fbefdad2
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2022-03-04 09:46:30 +0100

    Create 283. Move Zeroes.js

---
+++ 283. Move Zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    
+    let zeros = 0
+    
+    for(let i = 0; i < nums.length; i ++){
+        
+        if(nums[i] === 0){
+            
+            nums.splice(i, 1)
+            zeros ++
+            i --
+            
+        }
+        
+    }
+    
+    for(let i = 0; i < zeros; i ++)
+        nums.push(0)
+    
+}
